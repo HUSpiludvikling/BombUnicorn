374ff9b83d9df3826ae150d9cb6d2fee84fc93e9
Stine <sle3108@gmail.com>
2019-04-10 17:00:01 +0200

    endringer
    Nyt script
    og andre ting

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gamekit2D;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField]
+    Damageable damageable;
+
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        damageable = transform.parent.GetComponentInParent<Damageable>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateLocalScale();
+    }
+
+    private void UpdateLocalScale()
+    {
+        transform.localScale = new Vector3(damageable.CurrentHealth / damageable.startingHealth, transform.localScale.y, transform.localScale.z);
+    }
+}
