cec459a5731e409fa71bf6b3eea935b7efc86aad
Stine <sle3108@gmail.com>
2019-04-24 17:12:42 +0200

    <3

---
+++ Assets/#Scripts/Skyd.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Skyd : MonoBehaviour
+{
+    public GameObject Bullet;
+    public KeyCode FireKey;
+    [SerializeField] bool facingRight = true;
+    SpriteRenderer playerSR;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerSR = GetComponent<SpriteRenderer>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        facingRight = !playerSR.flipX;
+        
+        if (Input.GetKeyDown(FireKey))
+        {
+            GameObject temp;
+
+            temp = Instantiate(Bullet, transform.position + (facingRight ? Vector3.right : Vector3.left) + Vector3.up, Quaternion.identity);
+            temp.GetComponent<Rigidbody2D>().AddForce((facingRight ? Vector2.right : Vector2.left), ForceMode2D.Impulse);  
+        }
+    }
+}
